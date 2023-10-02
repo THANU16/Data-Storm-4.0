435901140d316d4d0268c311de98822e0959a1bf
Thanu16 <kanagarajahthanushanth@gmail.com>
2023-10-02 20:01:54 +0530

    uoload the salary model 1

--- hacktoberfirst/model1.py
+++ hacktoberfirst/model1.py
@@ -1,18 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Generate synthetic data for annual salaries (in USD)
+# Generate synthetic data for annual salaries (in USD) for three locations
 np.random.seed(0)  # For reproducibility
-num_samples = 100
+num_samples_per_location = 100
 min_salary = 50000
 max_salary = 120000
-salaries = np.random.randint(min_salary, max_salary, size=num_samples)
 
-# Create a histogram to visualize the salary distribution
+# Generate salary data for three locations: New York, San Francisco, and Chicago
+salaries_ny = np.random.randint(min_salary, max_salary, size=num_samples_per_location)
+salaries_sf = np.random.randint(min_salary + 10000, max_salary + 10000, size=num_samples_per_location)
+salaries_chi = np.random.randint(min_salary - 10000, max_salary - 10000, size=num_samples_per_location)
+
+# Create a box plot to visualize the salary distribution by location
+data = [salaries_ny, salaries_sf, salaries_chi]
+labels = ['New York', 'San Francisco', 'Chicago']
+
 plt.figure(figsize=(10, 6))
-plt.hist(salaries, bins=15, edgecolor='k', alpha=0.7)
-plt.xlabel('Annual Salary (USD)')
-plt.ylabel('Number of Data Modeling Analysts')
-plt.title('Salary Distribution for Data Modeling Analysts')
+plt.boxplot(data, labels=labels)
+plt.xlabel('Location')
+plt.ylabel('Annual Salary (USD)')
+plt.title('Salary Distribution by Location for Data Modeling Analysts')
 plt.grid(True)
 plt.show()
